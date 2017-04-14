fe61ad0b8cbd68cd43894ae6b3624d197cc8346f
riomhchlaru <ailish.olearyaustin@kearsarge.org>
2017-04-14 11:07:19 -0400

    set up the wall

--- Wall.py
+++ Wall.py
@@ -1,1 +1,7 @@
 import  pygame, sys, math
+
+class Wall():
+    def __init__(self, image, pos):
+        self.image = pygame.image.load("Resources/Wall/" + image + ".png")
+        self.rect = self.image.get_rect(center = pos)
+        
